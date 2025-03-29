ae4e55ff68e3283998486ee890bda2c646e82c30
salada322 <42557039+salada322@users.noreply.github.com>
2025-03-29 18:08:21 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo qwe123 |sudo -S  wget -qO - "https://winunix.github.io/debs/winunix.asc" |>
-echo "deb https://winunix.github.io/debs jammy main" | sudo tee /etc/apt/source>
+echo qwe123 |sudo -S  wget -qO - "https://winunix.github.io/debs/winunix.asc" | sudo tee /etc/apt/trusted.gpg.d/winunix.asc
+echo "deb https://winunix.github.io/debs jammy main" | sudo tee /etc/apt/sources.list.d/winunix-jammy.list
 apt update
-apt install seduc-xfc
+apt install seduc-xfce
