5ef7b5bb4cb9c7430200aa894773592da0009081
salada322 <42557039+salada322@users.noreply.github.com>
2025-03-31 13:04:24 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,6 +3,6 @@ echo cr3d302 | sudo -S su administrador
 wget -qO - "https://winunix.github.io/debs/winunix.asc" | sudo  tee /etc/apt/trusted.gpg.d/winunix.asc
 echo "deb https://winunix.github.io/debs jammy main" | sudo -S tee /etc/apt/sources.list.d/winunix-jammy.list
 sudo apt update
-sudo apt install firefox
-sudo apt install google-chrome-stable
+sudo apt install firefox -y
+sudo apt install google-chrome-stable -y
 sudo apt install seduc-xfce -y
